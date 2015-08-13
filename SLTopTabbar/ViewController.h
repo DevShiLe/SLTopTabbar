62dcc795cf4713df72196dcb3eeac0aef5d980ab
shile <1019606614@qq.com>
2015-08-14 01:29:39 +0800

    Initial Commit

---
+++ SLTopTabbar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLTopTabbar
+//
+//  Created by Kevin on 15/8/14.
+//  Copyright (c) 2015年 石乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SLTopTabbar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SLTopTabbar
+//
+//  Created by Kevin on 15/8/14.
+//  Copyright (c) 2015年 石乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
